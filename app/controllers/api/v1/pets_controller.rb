1093805980fc28483f176f1a771d321f7ecdfe44
github username <github email address>
2017-07-11 17:47:58 -0400

    filter API request works for species and sex (thanks Jason)

--- app/controllers/api/v1/pets_controller.rb
+++ app/controllers/api/v1/pets_controller.rb
@@ -4,12 +4,20 @@ class Api::V1::PetsController < ApplicationController
     render json: pets
   end
 
+  def create
+
+    species = params[:species] || '%'
+    sex = params[:sex] || '%'
+    city = params[:city] || '%'
+    size = params[:size] || '%'
+    age = params[:age] || '%'
+    pets = Pet.by_species(species).by_sex(sex)
+
+    render json: pets
+  end
+
   def show
     pet = Pet.find_by(id: params[:id])
     render json: pet
-  end
-
-  def search
-    form_i = params[:form] || nil
   end
 end
--- app/models/pet.rb
+++ app/models/pet.rb
@@ -2,4 +2,12 @@ class Pet < ApplicationRecord
   belongs_to :shelter, :foreign_key => :petfinder_shelterId, :primary_key => :petfinder_shelterId
   has_many :user_pets
   has_many :users, through: :user_pets
+
+  def self.by_species(species)
+    self.where("species LIKE '#{species}'")
+  end
+
+  def self.by_sex(sex)
+    self.where("sex LIKE '#{sex}'")
+  end
 end
