eef945172aaa1902d81729b3f8d6b27984df3f26
github username <github email address>
2017-09-14 09:02:17 -0400

    added auth to user model

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -1,2 +1,26 @@
 class Api::V1::AuthController < ApplicationController
   before_action :authorize_user!, only: [:show]
+
+  def show
+    render json: {
+      id: current_user.id,
+      name: current_user.name
+    }
+  end
+
+  def create
+    user = User.find_by(name: params[:name])
+
+    if user.present? && user.authenticate(params[:password])
+      render json: {
+        id: user.id,
+        name: user.name,
+        jwt: JWT.encode({ user_id: user.id }, ENV['JWT_SECRET'], ENV['JWT_ALGORITHM'])
+      }
+    else
+      render json: {
+        error: 'Username or password incorrect'
+      }, status: 404
+    end
+  end
+end
