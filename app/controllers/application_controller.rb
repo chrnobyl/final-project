547f2c58fcf70afecc47c352a36ad878dfddbe0f
github username <github email address>
2017-09-14 22:02:29 -0400

    more auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,4 +11,25 @@ class ApplicationController < ActionController::API
   def current_user
     @current_user ||= User.find_by(id: user_id)
   end
+
+  def user_id
+    decoded_token.first['user_id']
+  end
+
+  def decoded_token
+    if token
+      begin
+        JWT.decode(token, ENV['JWT_SECRET'], true, { algorithm: false })
+      rescue JWT::DecodeError
+        return [{}]
+      end
+    else
+      [{}]
+    end
+  end
+
+  def token
+    request.headers['Authorization']
+  end
+  
 end
